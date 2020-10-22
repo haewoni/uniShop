3776d9e238c5fb25075d02ea9747ee65bced8cb3
tnghksdld93 <tnghksdld93@naver.com>
2020-10-23 04:25:41 +0900

    jumun

--- src/main/java/com/itwill/unishop/repository/JumunRepository.java
+++ src/main/java/com/itwill/unishop/repository/JumunRepository.java
@@ -1,5 +1,19 @@
 package com.itwill.unishop.repository;
 
-public interface JumunRepository {
+import java.util.ArrayList;
 
+import com.itwill.unishop.domain.Jumun;
+
+public interface JumunRepository {
+	
+	public int insertJumun(Jumun jumun) throws Exception;
+	
+	public ArrayList<Jumun> selectAll() throws Exception;
+	
+	public Jumun selectById(String member_id) throws Exception;
+	
+	public Jumun updateJumunByNo(Jumun jumun) throws Exception;
+	
+	public int deleteJumunByNo(Integer jumun_no) throws Exception;
+	
 }
