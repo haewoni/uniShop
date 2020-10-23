75f6b1b2b9cdf1b0f1e08ec93283b05183e006cc
eyoonho54 <eyoonho@naver.com>
2020-10-23 16:56:16 +0900

    delivery repository

--- src/main/java/com/itwill/unishop/repository/DeliveryRepositoryImpl.java
+++ src/main/java/com/itwill/unishop/repository/DeliveryRepositoryImpl.java
@@ -1,43 +1,60 @@
 package com.itwill.unishop.repository;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.itwill.unishop.domain.Delivery;
+import com.itwill.unishop.mapper.DeliveryMapper;
 
 public class DeliveryRepositoryImpl implements DeliveryRepository{
 	
-	public final static String NAMESPACE = "com.itwill.unishop.mapper.DeliveryMapper.";
+	private SqlSessionFactory sqlSessionFactory;
 	
 	public DeliveryRepositoryImpl() throws Exception{
-		
+		InputStream in=null;
+		try {
+			in=Resources.getResourceAsStream("application-config.xml");
+			this.sqlSessionFactory = 
+					new SqlSessionFactoryBuilder()
+					.build(in);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public int insertDelivery(Delivery delivery) {
-		
-		return 0;
+		int rowcount = 0;
+		rowcount = sqlSessionFactory.openSession(true).getMapper(DeliveryMapper.class).insertDelivery(delivery);
+		return rowcount;
 	}
 	@Override
 	public int updateDelivery(String delivery_no) {
-		
-		return 0;
+		int rowcount = 0;
+		rowcount = sqlSessionFactory.openSession(true).getMapper(DeliveryMapper.class).updateDelivery(delivery_no);
+		return rowcount;
 	}
 	@Override
 	public int deleteDelivery(String delivery_no) {
-		
-		return 0;
+		int rowcount = 0;
+		rowcount = sqlSessionFactory.openSession(true).getMapper(DeliveryMapper.class).deleteDelivery(delivery_no);
+		return rowcount;
 	}
 	@Override
 	public Delivery selectByNo(String delivery_no) {
-		
-		return null;
+		Delivery delivery = new Delivery();
+		delivery = sqlSessionFactory.openSession(true).getMapper(DeliveryMapper.class).selectByNo(delivery_no);
+		return delivery;
 	}
 	@Override
 	public ArrayList<Delivery> selectAll() {
-		
-		return null;
+		ArrayList<Delivery> deliveryList = new ArrayList<Delivery>();
+		deliveryList = (ArrayList<Delivery>) sqlSessionFactory.openSession(true).getMapper(DeliveryMapper.class).selectAll();
+		return deliveryList;
 	}
 
 	
