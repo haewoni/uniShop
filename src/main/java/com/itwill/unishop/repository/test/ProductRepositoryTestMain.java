15c82169582131e342f6aa1464ceea653fc48ec5
haewoni <suzyhw96@gmail.com>
2020-10-26 00:59:40 +0900

    import 에러 변경

--- src/main/java/com/itwill/unishop/repository/test/ProductRepositoryTestMain.java
+++ src/main/java/com/itwill/unishop/repository/test/ProductRepositoryTestMain.java
@@ -4,7 +4,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.itwill.unishop.domain.Product;
-import com.itwill.unishop.repository.AddressRepository;
 import com.itwill.unishop.repository.ProductRepository;
 
 
