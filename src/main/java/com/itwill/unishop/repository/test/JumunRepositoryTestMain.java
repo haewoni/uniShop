6e9da7eab300e20eb3a7a1341bd59f04546c6780
tnghksdld93 <tnghksdld93@naver.com>
2020-10-26 22:55:45 +0900

    jumun service

--- src/main/java/com/itwill/unishop/repository/test/JumunRepositoryTestMain.java
+++ src/main/java/com/itwill/unishop/repository/test/JumunRepositoryTestMain.java
@@ -15,35 +15,35 @@ public class JumunRepositoryTestMain {
 		JumunRepository jumunRepository = (JumunRepository) applicationContext.getBean("jumunRepositoryImpl");
 		
 		/********************** insert **********************/
-		System.out.println(jumunRepository.insertJumun(
-				new Jumun(-9999, new Date(), "waiting", 77777, "1346 7964 2585 1973", "06/23", "789", "김민정", "uni3", "EX")));
-		System.out.println();
-		System.out.println(jumunRepository.insertJumun(
-				new Jumun(-9999, new Date(), "waiting", 88888, "1346 7964 2585 1973", "06/23", "789", "김민정", "uni3", "GEN")));
+//		System.out.println(jumunRepository.insertJumun(
+//				new Jumun(-9999, new Date(), "waiting", 77777, "1346 7964 2585 1973", "06/23", "789", "김민정", "uni3", "EX")));
+//		System.out.println();
+//		System.out.println(jumunRepository.insertJumun(
+//				new Jumun(-9999, new Date(), "waiting", 88888, "1346 7964 2585 1973", "06/23", "789", "김민정", "uni3", "GEN")));
 		/*
 		 	번호는 순서대로, 주문하는 날짜로 생성하고 
 		 */
 		
 		/********************** select **********************/
-		System.out.println(jumunRepository.selectById("uni1"));
-		System.out.println();
-		System.out.println(jumunRepository.selectById("uni2"));
-		System.out.println();
-		System.out.println(jumunRepository.selectById("uni3"));
-		System.out.println();
-		
-		System.out.println(jumunRepository.selectAll());
+//		System.out.println(jumunRepository.selectById("uni1"));
+//		System.out.println();
+//		System.out.println(jumunRepository.selectById("uni2"));
+//		System.out.println();
+//		System.out.println(jumunRepository.selectById("uni3"));
+//		System.out.println();
+//		
+//		System.out.println(jumunRepository.selectAll());
 		
 		
 		
 		/********************** update **********************/
-		Jumun jumun1 = new Jumun(3, new Date(), "waiting", 987654, "1111 2222 3333 8888", "01/26", "222", "수환잉", "uni3", "EX");
+		Jumun jumun1 = new Jumun(3, new Date(), "waiting", 987654, "1111 2222 3333 8888", "01/26", "222", "수환잉", "uni3", "GEN");
 		System.out.println(jumunRepository.updateJumun(jumun1));
 		
 		
 		
 		/********************** delete **********************/
-		System.out.println(jumunRepository.deleteJumunByNo(3));
+//		System.out.println(jumunRepository.deleteJumunByNo(3));
 		
 	}
 
