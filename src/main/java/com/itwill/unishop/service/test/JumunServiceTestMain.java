fa4977d579ebf976e8dc8916bd07e8376735b99d
tnghksdld93 <tnghksdld93@naver.com>
2020-10-27 20:46:07 +0900

    service test 한번 더 확인

--- src/main/java/com/itwill/unishop/service/test/JumunServiceTestMain.java
+++ src/main/java/com/itwill/unishop/service/test/JumunServiceTestMain.java
@@ -14,13 +14,23 @@ public class JumunServiceTestMain {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/application-config.xml");
 		JumunService jumunService = (JumunService) applicationContext.getBean("jumunServiceImpl");
 		
-		System.out.println(jumunService.insertJumun(
-				new Jumun(-9999, new Date(), "Waiting", 88800, "1346 7964 2585 1973", "06/23", "789", "김민정", "uni3", "GEN")));
-		Jumun jumun = new Jumun(-9999, new Date(), "Canceled", 15151, "1346 7964 2585 1973", "06/23", "789", "김민정", "uni3", "EX");
-		System.out.println(jumunService.updateJumun(jumun));
-		System.out.println(jumunService.selectAll());
-		System.out.println(jumunService.selectById("uni3"));
-		System.out.println(jumunService.deleteJumunByNo(5));
+		System.out.println("--------------------------------------------- 1. INSERT ---------------------------------------------");
+//		System.out.println(jumunService.insertJumun(
+//				new Jumun(-9999, new Date(), "Waiting", 88800, "1346 7964 2585 1973", "06/23", "789", "김민정", "uni3", "GEN")));
+//		Jumun jumun1 = new Jumun(-9999, new Date(), "Waiting", 15151, "1111 2222 3333 4444", "12/12", "987", "정수환", "uni4", "EX");
+//		System.out.println(jumunService.insertJumun(jumun1));
+		
+		System.out.println("--------------------------------------------- 2. UPDATE ---------------------------------------------");
+//		System.out.println(jumunService.updateJumun(new Jumun(5, new Date(), "Canceled", 212121, "1111 2222 3333 4444", "12/12", "987", "신지수", "uni4", "EX")));
+		
+		System.out.println("--------------------------------------------- 3. SELECT ALL ---------------------------------------------");
+//		System.out.println(jumunService.selectAll());
+		
+		System.out.println("--------------------------------------------- 4. SELECT BY ID ---------------------------------------------");
+//		System.out.println(jumunService.selectById("uni3"));
+		
+		System.out.println("--------------------------------------------- 5. DELETE ---------------------------------------------");
+//		System.out.println(jumunService.deleteJumunByNo(5));
 	}
 
 }
--- src/main/java/com/itwill/unishop/service/test/Jumun_DetailServiceTestMain.java
+++ src/main/java/com/itwill/unishop/service/test/Jumun_DetailServiceTestMain.java
@@ -12,13 +12,22 @@ public class Jumun_DetailServiceTestMain {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/application-config.xml");
 		Jumun_DetailService jumun_DetailService = (Jumun_DetailService) applicationContext.getBean("jumun_DetailServiceImpl");
 		
-		System.out.println(jumun_DetailService.insertJumunDetail(
-				new Jumun_Detail(-9999, "PALACE", "L", 199000, "3", 3, "432733")));
-		System.out.println(jumun_DetailService.updateJumunDetail(
-				new Jumun_Detail(-9999, "BAPE", "M", 120000, "1", 3, "432733")));
-		System.out.println(jumun_DetailService.selectAll());
+		System.out.println("--------------------------------------------- 1. INSERT ---------------------------------------------");
+//		System.out.println(jumun_DetailService.insertJumunDetail(
+//				new Jumun_Detail(-9999, "PALACE", "L", 199000, "3", 3, "432733")));
+		
+		System.out.println("--------------------------------------------- 2. UPDATE ---------------------------------------------");
+//		System.out.println(jumun_DetailService.updateJumunDetail(
+//				new Jumun_Detail(-9999, "BAPE", "M", 120000, "1", 2, "432087")));
+//		
+//		System.out.println("--------------------------------------------- 3. SELECT ALL ---------------------------------------------");
+//		System.out.println(jumun_DetailService.selectAll());
+//		
+//		System.out.println("--------------------------------------------- 4. SELECT BY JUMUN_NO ---------------------------------------------");
 		System.out.println(jumun_DetailService.selectByNo(3));
-		System.out.println(jumun_DetailService.deleteJumunDetail(2));
+//		
+//		System.out.println("--------------------------------------------- 5. DELETE ---------------------------------------------");
+//		System.out.println(jumun_DetailService.deleteJumunDetail(7));
 		
 	}
 
