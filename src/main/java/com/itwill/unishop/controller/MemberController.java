673f3f9385b31d339a72135d362c34ad7abed666
bnghwhdgh <bnghwhdgh@gmail.com>
2020-10-28 19:18:03 +0900

    member 컨트롤러 예시 작성중 예시 업로드 서둘러 하겠습니다

---
+++ src/main/java/com/itwill/unishop/controller/MemberController.java
@@ -0,0 +1,67 @@
+package com.itwill.unishop.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.itwill.unishop.domain.Member;
+import com.itwill.unishop.exception.MemberNotFoundException;
+import com.itwill.unishop.exception.PasswordMismatchException;
+import com.itwill.unishop.service.MemberService;
+
+@Controller
+public class MemberController {
+
+	@Autowired
+	private MemberService memberService;
+
+	@RequestMapping("/member_login_form")
+	public String member_login_form() {
+		return "member_login_form"; 
+	}
+
+	@RequestMapping(value = "/member_login_action", method = RequestMethod.GET)
+	public String member_login_Action_GET() {
+		return "member_login_form"; 
+	}
+	@RequestMapping(value = "/member_login_action", method = RequestMethod.POST)
+	public String member_login_action_POST(HttpServletRequest request,@RequestParam String member_id, @RequestParam String member_password) {
+		String forwardPath = "";
+		try {
+			HttpSession session = request.getSession();
+			Member loginMember=memberService.loginMember(member_id, member_password);
+			session.setAttribute("sUser", loginMember);
+			session.setAttribute("sUserId", member_id);
+			forwardPath = "redirect:member_login_form";
+		} catch (PasswordMismatchException e) {
+			
+			e.printStackTrace();
+		}catch(MemberNotFoundException e){
+			
+			e.printStackTrace();
+		}
+		return forwardPath;
+	}
+	
+	public String member_profile() {
+		
+		return "";
+	}
+
+
+
+	//모든 Exception을 던지면 이곳으로 날라온다
+	@ExceptionHandler(Exception.class)
+	public String member_error_handle(Exception e) {
+		return "404.html"; //예시입니다. 후에 .html은 뺄거예요
+	}
+
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<a href="member_login_form">멤버 로그인 폼입니다.</a>
+</body>
+</html>
