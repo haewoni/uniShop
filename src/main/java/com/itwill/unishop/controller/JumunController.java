bbcb71d9475e465c653de0e2a53382c43ae630e7
cjy2815 <cjy2815@naver.com>
2020-11-02 15:13:52 +0900

    log4j 제거, jumun 수정

--- src/main/java/com/itwill/unishop/controller/JumunController.java
+++ src/main/java/com/itwill/unishop/controller/JumunController.java
@@ -1,6 +1,7 @@
 package com.itwill.unishop.controller;
 
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +21,7 @@ import com.itwill.unishop.domain.Jumun;
 import com.itwill.unishop.domain.Delivery;
 
 import com.itwill.unishop.domain.Member;
-
+import com.itwill.unishop.domain.Product;
 import com.itwill.unishop.domain.Review;
 import com.itwill.unishop.exception.MemberNotFoundException;
 import com.itwill.unishop.exception.PasswordMismatchException;
@@ -97,11 +98,17 @@ public class JumunController {
 	}
 	@RequestMapping(value = "/jumun_payment_action", method = RequestMethod.POST)
 	public String jumun_payment_action_POST(Model model,HttpSession session, @ModelAttribute Jumun jumun) {
-		Member loginMember = (Member)session.getAttribute("loginMember");
-		session.setAttribute("loginMember", new Member("uni4", "1111", "tttt", "1111","ttt@naver.com"));
+		//Member loginMember = (Member)session.getAttribute("loginMember");
+		//session.setAttribute("loginMember", new Member("uni4", "1111", "tttt", "1111","ttt@naver.com"));
+		Jumun jumun2=new Jumun();
+		jumun2.setCard_no(jumun.getCard_no());
+		jumun2.setCard_expire_date(jumun.getCard_expire_date());
+		jumun2.setCard_cvc(jumun.getCard_cvc());
+		jumun2.setCard_member_name(jumun.getCard_member_name());
+		jumun2.setMember_id(jumun.getMember_id());
 		String forwardPath = " ";
-		jumunService.updateJumunCard(jumun);
 		forwardPath="jumun_review_form";
+		//System.out.println(jumun2);
 		return forwardPath;
 	}
 	/*
@@ -118,6 +125,8 @@ public class JumunController {
 	@RequestMapping(value = "/jumun_review_action", method = RequestMethod.POST)
 	public String jumun_review_action_POST(Model model,@ModelAttribute Review review) throws Exception {
 		String forwardPath = " ";
+		//Member member = memberService.selectAddressById(member_id);
+		//model.addAttribute("member",member);
 		reviewService.insertReview(review);
 		forwardPath="jumun_review_form";
 		return forwardPath;
--- src/main/webapp/WEB-INF/views/jumun_payment_form.jsp
+++ src/main/webapp/WEB-INF/views/jumun_payment_form.jsp
@@ -1,31 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<title>Checkout</title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-</head>
-<body bgcolor=#FFFFFF text=#000000 leftmargin=0 topmargin=0
-	marginwidth=0 marginheight=0>
-		
-<div id="content">
-	<table width=0 border=0 cellpadding=0 cellspacing=0>
-		<tr>
-			<td>
-				<!--contents--> <br />
-				<table style="padding-left: 10px" border=0 cellpadding=0
-					cellspacing=0>
-					<tr>
-						<td bgcolor="f4f4f4" height="22">&nbsp;&nbsp;<b>payment</b></td>
-					</tr>
-				</table> <!-- payment write form  -->
-				<form name="f" method="post" action="jumun_payment_action">
-				<!-- 
-					<input type="hidden" name="member_id"  />
-					 -->
-					<table border="0" cellpadding="0" cellspacing="1" width="590"
-						bgcolor="BBBBBB">
+	pageEncoding="UTF-8"%>
+					<form name="f" method="POST" action="jumun_payment_action">
+
+						<table border="0" cellpadding="0" cellspacing="1" width="590"
+							bgcolor="BBBBBB">
+
+				
 						<tr>
 							<td width=100 align=center bgcolor="E6ECDE" height="22">카드 번호</td>
 							<td width=490 align="left" bgcolor="ffffff"
@@ -62,21 +42,10 @@
 			
 						
 					</table>
-					<input type="submit" > 
-				</form> <br />
-				<table width=590 border=0 cellpadding=0 cellspacing=0>
-					<tr>
-						<td align=center>
-						
-					</tr>
-				</table>
-			</td>
-		</tr>
-	</table>
-</div>
+						<input type="submit">
+					</form> <br />
 
-		
-	
+
 	<!--container end-->
 </body>
 </html>
